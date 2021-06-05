20180a56a94e4a48447a8d305d19d47091750126
nakashi120 <nakashi120@gmail.com>
2021-06-06 00:55:09 +0900

    Added endpoint for authentication and users

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -21,7 +21,7 @@ class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
     email = serializers.CharField()
     password = serializers.CharField(
-        style={'input_type', 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False
     )
 
